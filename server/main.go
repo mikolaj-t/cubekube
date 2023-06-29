989735a9d6d2498a9db26e9f75caa05ffff12a90
mikolaj-t <51460555+mikolaj-t@users.noreply.github.com>
2023-06-29 12:51:06 +0200

    Add basic server repsonse

--- server/main.go
+++ server/main.go
@@ -1,12 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	server := http.Server{}
-	_ = server.ListenAndServe()
-
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+	server := http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		Addr:              ":8080",
+		Handler:           mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalln(err)
+	}
 	defer func() {
 		_ = server.Close()
 	}()
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello there!")
 }
